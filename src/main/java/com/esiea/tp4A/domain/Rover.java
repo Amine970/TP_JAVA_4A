7286bdbcb4f24e4c94050d3fd0208726d9df817f
IronCommander <46050205+IronCommander@users.noreply.github.com>
2020-04-12 17:40:58 +0200

    Add Final Part Rover
    Ajout de la classe Rover finalisée avec la lecture des commandes et leur gestion pour effectuer les différentes actions.

--- src/main/java/com/esiea/tp4A/domain/Rover.java
+++ src/main/java/com/esiea/tp4A/domain/Rover.java
@@ -24,4 +24,29 @@ public class Rover implements Position,MarsRover {
 		if(getY() < -Math.round(map.getTailleY()/2)+1) Coor = Position.of(getX(),Math.round(map.getTailleY()/2),getDirection());
 		if(getX() > Math.round(map.getTailleX()/2)) Coor = Position.of(-Math.round(map.getTailleX()/2)+1,getY(),getDirection());
 		if(getX() < -Math.round(map.getTailleX()/2)+1) Coor = Position.of(Math.round(map.getTailleX()/2),getY(),getDirection());	}
+	private void deplacement(char com) {
+		Move move = new Move(map,getDirection(),getX(),getY());
+		move.go(com);
+		Coor = Position.of(move.setX(),move.setY(),move.setDirection());
+		if(com == 'f' || com == 'b') cercle();}
+	private void action(char com[]) {
+		for(int i=0;i<com.length;i++) {
+			if(com[i] == 's') new Tir(map,getX(),getY(),getDirection(),range);
+			else deplacement(com[i]);}}
+	public Position move(String commande) {
+		char [] com = commande.toCharArray();
+		if(vie) action(com);
+        return Coor;    } 
+	public String getName() {
+		return name;
+	}
+	public boolean getLive() {
+		return vie;
+	}
+	public void initCoor(int x, int y) {
+		Coor = Position.of(x,y,Direction.NORTH);
+	}
+	public void mort() {
+		vie = false;
+	}
 }
