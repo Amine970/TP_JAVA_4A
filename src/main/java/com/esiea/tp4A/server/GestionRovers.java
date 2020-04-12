c3672f7abbc75fa20712616fd5e3a5da91b14c25
IronCommander <46050205+IronCommander@users.noreply.github.com>
2020-04-12 17:49:08 +0200

    Add Final Part GestionRovers
    Ajout de la classe GestionRovers finalisée avec les méthodes renvoyant la liste des obstacles détectés, celle des joueurs détectés et la méthode renvoyant le statut du joueur.

--- src/main/java/com/esiea/tp4A/server/GestionRovers.java
+++ src/main/java/com/esiea/tp4A/server/GestionRovers.java
@@ -19,10 +19,27 @@ public class GestionRovers implements RoversServer {
 		GenerateRover rov = new GenerateRover(map, Liste);
 		Liste.add(rov.genStart(laserRange,player));	
 	}
+	public ArrayList<Rover> getMapRover(int x, int y) {
+		ArrayList<Rover> Rov = new ArrayList<Rover>();
+		for(int i=0;i<Liste.size();i++) if(Math.abs(Liste.get(i).getX()-x) <= 30 && Math.abs(Liste.get(i).getY()-y) <= 30) Rov.add(Liste.get(i));
+		return Rov;
+	}
+	public String getStatus(String player) {
+		Rover tmp = getRover(player);
+		if(tmp.getLive()) return "En vie";
+		return "Mort";
+	}
 
+	public ArrayList<Position> getObstacle(int x, int y) {
+		ArrayList<Position> obstacle = map.getObstacle();
+		ArrayList<Position> radar = new ArrayList<Position>();
+		for(int i=0;i<obstacle.size();i++) if(Math.abs(obstacle.get(i).getX()-x) <= 30 && Math.abs(obstacle.get(i).getY()-y) <= 30) radar.add(obstacle.get(i));
+		return radar;
+	}
 	public boolean containRover(String player) {
 		for(int i=0;i<Liste.size();i++) if(Liste.get(i).getName().equals(player)) return true;
 		return false;
 	}
 
 }
+
