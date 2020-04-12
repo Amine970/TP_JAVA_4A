9a68e93af1993a0cc8f0598af2af40f5d0d688b4
clara1245 <44846451+clara1245@users.noreply.github.com>
2020-04-12 19:12:33 +0200

    Update Launch.java
    + possibilité de démarrer le serveur avec les paramètres aléatoires

--- src/main/java/com/esiea/tp4A/server/Launch.java
+++ src/main/java/com/esiea/tp4A/server/Launch.java
@@ -4,9 +4,13 @@ import java.io.IOException;
 import java.util.Random;
 
 public class Launch {
+	private static int choixTaille[] = {100,300,600};
+	private static int choixRange[] = {5,30,1000};
     public static void main (String []args) throws IOException {
     	Random random = new Random();
-        Server serveur = new Server(8086, 10,10,2);
+    	int i = random.nextInt(3);
+    	int j = random.nextInt(3);
+        Server serveur = new Server(8086, choixTaille[i],choixTaille[i],choixRange[j]);
         serveur.start();
     }
 }
