38df6ffe455dbda35953d7511de15dd54006f165
PaulineVlp <45972903+PaulineVlp@users.noreply.github.com>
2020-04-12 19:32:58 +0200

    + générer le rover de façon aléatoire sur la carte

--- src/main/java/com/esiea/tp4A/server/GenerateRover.java
+++ src/main/java/com/esiea/tp4A/server/GenerateRover.java
@@ -13,6 +13,20 @@ public class GenerateRover {
 		this.Liste = Liste;
 	}
 	
+	private boolean testPos(int x, int y) {
+		if(map.valid(x,y)) {for(int i=0;i<Liste.size();i++) {if(Liste.get(i).getX()==x && Liste.get(i).getY()==y && Liste.size() > 0) return false;}}
+		else return false;
+		return true;
+	}
+	
+	private int[] genPos(int []coor, Random random) {
+		do {
+			coor[0] = random.nextInt(map.getTailleX()/2)-1;
+			coor[1] = random.nextInt(map.getTailleY()/2)-1;
+		} while(!testPos(coor[0],coor[1]));
+		return coor;
+	}
+	
 	public Rover genStart(int laserRange, String player) {
 		Random random = new Random();
 		int []coor = new int[2];
