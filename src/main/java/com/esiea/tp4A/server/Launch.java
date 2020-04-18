eaa5391953a946e5ced4d91083ced80941bd7bdd
IronCommander <46050205+IronCommander@users.noreply.github.com>
2020-04-19 01:47:59 +0200

    Change variable type
    Retrait de variables non final

--- src/main/java/com/esiea/tp4A/server/Launch.java
+++ src/main/java/com/esiea/tp4A/server/Launch.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 import java.util.Random;
 
 public class Launch {
-	private static int choixTaille[] = {100,300,600};
-	private static int choixRange[] = {5,30,1000};
     public static void main (String []args) throws IOException {
+        int choixTaille[] = {100,300,600};
+        int choixRange[] = {5,30,1000};
     	Random random = new Random();
     	int i = random.nextInt(3);
     	int j = random.nextInt(3);
